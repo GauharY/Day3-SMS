c6862187ed26bcf676cb852caceeefd6ec43a2d7
Gauhar Yadav <gauharyadav@gmail.com>
2026-01-27 15:39:03 +0530

    CRUD operations

--- src/main/java/com/example/day3sms/service/StudentService.java
+++ src/main/java/com/example/day3sms/service/StudentService.java
@@ -3,6 +3,10 @@ package com.example.day3sms.service;
 import com.example.day3sms.model.StudentModel;
 import com.example.day3sms.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -15,5 +19,27 @@ public class StudentService {
     //    create
     public StudentModel addStudent(StudentModel student){
         return repository.save(student);
+    }
+
+//    Display Student
+    public List<StudentModel> getStudents(){
+        return repository.findAll();
+    }
+//    Update
+    public StudentModel updateStudent(String id, StudentModel student){
+        StudentModel existingStudent = repository.findById(id).orElseThrow(() -> new RuntimeException("Student Not Found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+        return repository.save(existingStudent);
+
+    }
+
+//    Delete
+    public void deleteStudent(String id){
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Student Not Found");
+        }
+        repository.deleteById(id);
     }
 }
